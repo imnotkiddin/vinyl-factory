4a9708c8bb18062dff74fe627849cf6cd3a2bb03
imnotkiddin <manuelsere14@gmail.com>
2022-01-20 11:27:02 -0300

    Creacion de la barra de Navegacion
    Integración de NavBar en REACTJS, fuentes e iconos en HTML y estilos en CSS. Queda pendiente agregar un boton con un modal para Contacto.

---
+++ src/components/NavBar/MenuItem.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "70s",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "80s",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "90s",
+    url: "#",
+    cName: "nav-links",
+  },
+];
